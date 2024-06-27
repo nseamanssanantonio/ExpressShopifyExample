510ec2cca4135e4de7101e4f7fb12f9d7776a686
nseamanssanantonio <noisetosignal2020@gmail.com>
2024-06-27 13:02:08 -0500

    Added more customer tables and foreign keys to table script

--- sqltables.sql
+++ sqltables.sql
@@ -27,6 +27,48 @@ CREATE TABLE user_history (
 	FOREIGN KEY (id_user) REFERENCES user_main(id_sql)
 );
 
+CREATE TABLE customer_main(
+	id_sql int,
+	first_name varchar(256),
+	last_name varchar(256),
+	customer_password varchar(32),
+	id_strip varchar(256),
+	PRIMARY KEY (id_sql)
+);
+
+CREATE TABLE customer_updates(
+	id_sql int,
+	id_customer int,
+	table_updated varchar(256),
+	data_update varchar(256),
+	data_previous varchar(256),
+	PRIMARY KEY (id_sql, id_customer),
+	FOREIGN KEY (id_customer) REFERENCES customer_main(id_sql)
+);
+
+CREATE TABLE customer_location(
+	id_sql int,
+	id_customer int,
+	address_one varchar(256),
+	address_two varchar(256),
+	zipcode varchar(10),
+	country varchar(256),
+	state varchar(256),
+	PRIMARY KEY (id_sql, id_customer, address_one, address_two, zipcode, country, state),
+	FOREIGN KEY (id_customer) REFERENCES customer_main(id_sql)
+);
+
+CREATE TABLE customer_purchase (
+	id_sql int,
+	id_customer int,
+	id_variant int, 
+	id_address int,
+	PRIMARY KEY (id_sql, id_customer, id_variant, id_address),
+	FOREIGN KEY (id_variant) REFERENCES variant_main(id_sql),
+	FOREIGN KEY (id_customer) REFERENCES customer_main(id_sql),
+	FOREIGN KEY (id_address) REFERENCES customer_location(id_sql)
+);
+
 CREATE TABLE product_main (
 	id_sql int,
 	description varchar(10240),
@@ -46,7 +88,7 @@ CREATE TABLE product_updates (
 CREATE TABLE product_images (
 	id_sql int,
 	id_product int,
-	images varchar(2048)[],
+	images varchar(2048),
 	time_created timestamp,
 	PRIMARY KEY (id_sql, id_product),
 	FOREIGN KEY (id_product) REFERENCES product_main(id_sql)
